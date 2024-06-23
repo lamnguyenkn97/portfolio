e2633d6b80491fbb24708d5c75f27564129a0ad2
Lam Nguyen <languyen@axon.com>
2024-06-23 21:06:12 +0700

    Set up portfolio

--- portfolio/src/components/profile/profile.tsx
+++ portfolio/src/components/profile/profile.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import {Grid, Stack, Typography} from "@mui/material";
 import {NavBar} from "../navBar";
-import {SocialNetworks} from "./components";
+import {SocialNetworks} from "./components/socialNetworks";
 
 
 export const Profile = () => {
     return (
-        <Stack justifyContent={'space-between'} sx={{border: '1px red solid', height: '100%'}}>
+        <Stack alignItems={'center'} boxSizing={"border-box"} justifyContent={"space-between"}  sx={{border: '1px red solid', height: '100%', pt: 5, pb: 5}}>
             <Stack>
-                <Typography variant="h3" align="left">Lam Nguyen</Typography>
-                <Typography variant="h6" align="left">Frontend Developer</Typography>
+                <Typography fontWeight={'bold'} variant="h3" align="left">Lam Nguyen</Typography>
+                <Typography variant="h6" align="left" >Frontend Developer</Typography>
+                <Typography variant="body1" align="left" sx={{mb: 5}}>I build pixel-perfect, engaging, and accessible digital experiences.</Typography>
                 <NavBar />
             </Stack>
             <SocialNetworks />
